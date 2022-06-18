a65b136211aae4ec985cad18ab4faf4e0dcb6341
valderyjr <valderyjunior2002@gmail.com>
2022-06-18 19:49:12 -0300

    conectando ao banco de dados

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
+const db = require('./config/dbConnection')
+
+db.on("error", () => console.log("Erro na conexão."))
+db.once("open", () => console.log("Conectado ao banco."))
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
